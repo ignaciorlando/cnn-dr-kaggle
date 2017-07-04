c267e169a2ab6295f836ca49c327540265d90221
José Ignacio Orlando <ignaciorlando@gmail.com>
2017-07-04 15:52:31 -0300

    #8 - Almost done. We just have to control now the positional parameters

--- preprocess/main.py
+++ preprocess/main.py
@@ -6,11 +6,15 @@ from equalize_fundus_image_intensities import equalize_fundus_image_intensities
 import numpy as np
 
 
-def main(root_path, output_path, mask_threshold=0.01):
+def main(root_path, output_path, first_image=0, last_image=None, overwrite=False, mask_threshold=0.01):
     """
     This function runs the preprocessing filters to all images inside root_path/images
     and creates a separate folder in root_path/masks with the resulting FOVs.
     """
+
+    # check if first_image is lower than last_image
+    if not(last_image is None):
+        assert (first_image < last_image), 'first_image (%d) is higher than last_image (%d)' % (first_image, last_image)
 
     # By default, we assume that the input folder has a folder inside namely 'images'
     input_directory = path.join(root_path, 'images')
@@ -27,43 +31,62 @@ def main(root_path, output_path, mask_threshold=0.01):
     if not path.exists(output_directory_masks):
         makedirs(output_directory_masks)
 
-    # for each file in the folder
-    num_images = len(listdir(input_directory))
-    current_image = 0
-    for file_i in listdir(input_directory):
-        current_image = current_image + 1
-        # if it is a file...
-        if path.isfile(path.join(input_directory, file_i)):
-            print(file_i)
-            print('Processing image ' + str(current_image) + '/' + str(num_images), end="", flush=True)
+    # get all the files in the folder
+    all_filenames = listdir(input_directory)
 
-            # read the input image
-            image = io.imread(path.join(input_directory, file_i))
+    # check if the last image does not exceed the amount of available images
+    num_images = len(all_filenames)
+    if last_image is None:
+        last_image = num_images - 1
+    assert (last_image <= num_images), 'last_image (%d) is higher than the number of images (%d)' % (last_image, num_images)
+
+    # get only the images given by first_image and last_image
+    images_to_process = all_filenames[first_image:last_image+1]
+
+    # for each image in the folder
+    current_image = 0
+    num_images = last_image - first_image + 1
+    for file_i in images_to_process:
+
+        # (if it is a file...) and ((overwrite) or (not overwrite and the image was processed before ))
+        if path.isfile(path.join(input_directory, file_i)):
+
+            # move the counter of images
+            current_image = current_image + 1
             # prepare output file name for the mask
             mask_filename = file_i[:file_i.rfind('.')] + '.png'
 
-            # create the FOV mask associated to the image
-            print('\tCreating mask...', end="", flush=True)
-            # generate the fov mask
-            mask = get_fov_mask(image, mask_threshold)
+            # if we have to overwrite or if we don't by the mask doesn't exist
+            if overwrite or (not overwrite and not path.exists(path.join(output_directory_masks, mask_filename))):
 
-            # crop the images and the masks around the FOV
-            print('\tCropping around the FOV...', end="", flush=True)
-            preprocessed_image, mask = crop_fov_mask(image, mask)
+                print(file_i)
+                print('Processing image ' + str(current_image) + '/' + str(num_images), end="", flush=True)
 
-            # downsize the image and the mask
-            print('\Resizing the image...', end="", flush=True)
-            preprocessed_image = transform.resize(preprocessed_image, (512, 512, 3), preserve_range=True).astype(np.uint8)
-            mask = transform.resize(mask, (512, 512), order=0, preserve_range=True)
+                # read the input image
+                image = io.imread(path.join(input_directory, file_i))
 
-            # apply contrast equalization on the image
-            print('\tPreprocessing the image...', end="", flush=True)
-            preprocessed_image = equalize_fundus_image_intensities(preprocessed_image, mask)
+                # create the FOV mask associated to the image
+                print('\tCreating mask...', end="", flush=True)
+                # generate the fov mask
+                mask = get_fov_mask(image, mask_threshold)
 
-            # save the preprocessed image
-            io.imsave(path.join(output_directory_images, file_i), preprocessed_image)
-            # save the cropped mask
-            io.imsave(path.join(output_directory_masks, mask_filename), mask)
+                # crop the images and the masks around the FOV
+                print('\tCropping around the FOV...', end="", flush=True)
+                preprocessed_image, mask = crop_fov_mask(image, mask)
+
+                # downsize the image and the mask
+                print('\Resizing the image...', end="", flush=True)
+                preprocessed_image = transform.resize(preprocessed_image, (512, 512, 3), preserve_range=True).astype(np.uint8)
+                mask = transform.resize(mask, (512, 512), order=0, preserve_range=True)
+
+                # apply contrast equalization on the image
+                print('\tPreprocessing the image...', end="", flush=True)
+                preprocessed_image = equalize_fundus_image_intensities(preprocessed_image, mask)
+
+                # save the preprocessed image
+                io.imsave(path.join(output_directory_images, file_i), preprocessed_image)
+                # save the cropped mask
+                io.imsave(path.join(output_directory_masks, mask_filename), mask)
 
             #print('.', end="", flush=True)
 
@@ -75,14 +98,25 @@ def usage():
     print('ERROR: Usage: main.py <path-to-directory-of-image-to-be-processed> <output-path> <mask-threshold>')
 
 
-
+import argparse
 import sys
 
 if __name__ == '__main__':
+
+
     if len(sys.argv) < 3:
         usage()
         exit()
-    elif len(sys.argv) == 3:
-        main(sys.argv[1], sys.argv[2])
     else:
-        main(sys.argv[1], sys.argv[2], float(sys.argv[3]))
+        # lets create an argument parser to control the input parameters
+        parser = argparse.ArgumentParser()
+        parser.add_argument("input_directory", help="folder with the input images", type=str)
+        parser.add_argument("output_directory", help="folder with the output images", type=str)
+        parser.add_argument("-f", "--first_image", help="first image to process", type=int, default=0)
+        parser.add_argument("-l", "--last_image", help="last image to process", type=int, default=None)
+        parser.add_argument("-o", "--overwrite", help="indicate if we should overwrite the precomputed files", action="store_true")
+        parser.add_argument("-t", "--mask_threshold", help="threshold to compute the masks", type=float, default=0.01)
+        args = parser.parse_args()
+
+        # call the main function
+        main(args.input_directory, args.output_directory, args.first_image, args.last_image, args.overwrite, args.mask_threshold)
